e451e6c9a1565020cce0b84a33fef568fbb035e4
neeta <neetarizal@yahoo.com>
2024-02-13 08:29:48 -0600

    adding files to github

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "accesskey" {
+	default="" 
+}
+
+variable "secretkey" {
+	default=""
+}
+
+variable "vpcID" {
+	default="vpc-041bf523d5b8fedde"
+}
+variable "amiID" {
+	default="ami-0fe630eb857a6ec83"
+}
+variable "keyPair" {
+	default="toncat-key"
+}
+variable "keyPair1" {
+	default="toncat-key.pem"
+}
+
+
+
+
